cfd1fc6559f8b56df38f1172b9c9e0ca7ced33de
Vipul Shah <vipshah@microsoft.com>
2017-09-03 19:54:04 +0530

    Sample Assignment

---
+++ Vipul_Assignment_Fragment/app/src/main/java/com/microsoft/vipul_assignment_fragment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.microsoft.vipul_assignment_fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
